c29d6185d4ba0fe9e75c6caf48fd32515382cafb
braj011 <bhagyaraj01@gmail.com>
2018-07-16 18:17:46 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+
+function addElementToBeginningOfArray(array, element) {
+  var new_array = [element, array[0]]
+  return new_array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  
+}
+
+/* SOLVE THIS ONE
+function addElementToEndOfArray(array, element) {
+  var new_array = [array, ...element]
+  return new_array
+
+*/
+  
+}
+// THIS ONE IS is FINE!
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
